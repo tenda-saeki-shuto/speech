af13dc6aa79cb64b7e22357a9c2e1b9fbe8f3bf9
shooot223 <shuto2023@gmail.com>
2025-05-24 10:09:09 +0900

    make screen

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>ヒットアンドブロー</title>
 </head>
 <body>
+    <h1></h1>ヒットアンドブロー</h1>
+    <p>ヒットアンドブローは、数字を当てるゲームです。</p>
+    <p>4桁の数字を当ててください。</p>
+
+    <form action="game.php" method="post">
+        <label for="guess">あなたの予想:</label>
+        <input type="text" id="guess" name="guess" required pattern="\d{4}" title="4桁の数字を入力してください">
+        <button type="submit">送信</button>
+    </form>
 
 </body>
 </html>
