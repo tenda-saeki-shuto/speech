f116e44cc90494cc4a9021b08876110580b99ec1
shooot223 <shuto2023@gmail.com>
2025-05-25 10:09:53 +0900

    ランキングに登録するボタンを作成

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+session_start(); // セッションを有効化
+
+if (!isset($_SESSION['username'])) {
+    $_SESSION['username'] = "guest";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,6 +39,7 @@
             </tr>
         </table>
         <p id="message"></p>
+        <button id="ranking" style="display:none;">ランキングに登録する</button>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script>
@@ -45,11 +55,37 @@
             }).done(function(data) {
                 if (data.result === "success") {
                     alert("ヒット: " + data.hit + ", ブロー: " + data.blow);
-                    alert(data.message);
+                    $("#message").text(data.message);
                     $("#count").append("<td>" + data.count + "</td>");
                     $("#player_num").append("<td>" + data.player_num + "</td>");
                     $("#hit").append("<td>" + data.hit + "</td>");
                     $("#blow").append("<td>" + data.blow + "</td>");
+
+                    // ユーザーが正解した場合の処理(ランキングに登録するボタンを表示)
+                    if (data.hit === 4) {
+                        $("#ranking").text("ランキングに登録する");
+                        $("#ranking").show();
+                        $("#ranking").on("click", function() {
+                            $.post({
+                                url: "ranking.php",
+                                data: { player_num: p_num },
+                                dataType: "json",
+                            }).done(function(rankingData) {
+                                if (rankingData.result === "success") {
+                                    alert("ランキングに登録されました。");
+                                    location.reload(); // ページをリロードしてランキングを更新
+                                } else {
+                                    alert("エラー: " + rankingData.message);
+                                }
+                            }).fail(function() {
+                                alert("ランキング登録に失敗しました。");
+                            });
+                        });
+                    } else {
+                        $("#ranking").hide(); // 正解でない場合はボタンを非表示
+                    }
+
+
                 } else {
                     alert("エラー: " + data.message);
                 }
@@ -58,6 +94,8 @@
             });
         })
     })
+
+
 
     // ページがリロードされたときにセッションを破壊する
     $(window).on("beforeunload", function () {
