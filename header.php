12ba45e856359ae2080332c5e741198e2b4d8811
shooot223 <shuto2023@gmail.com>
2025-05-25 13:47:41 +0900

    リンク先を修正しました

--- header.php
+++ header.php
@@ -8,7 +8,7 @@ $user_name = $_SESSION['username'];
 
 <?php if ($user_name === "guest") : ?>
     <p>ゲストユーザーとしてプレイしています。<br>
-    <a href="login.php">ログイン</a>または<a href="register.php">新規登録</a>してください。</p>
+    <a href="login.php">ログイン</a>または<a href="registration.php">新規登録</a>してください。</p>
 <?php
 else : ?>
     <p>ようこそ、<?php echo htmlspecialchars($user_name); ?>さん！</p>
