725499c9491f422651c3d5513759e44ae0e3357e
shooot223 <shuto2023@gmail.com>
2025-05-25 15:45:07 +0900

    表示する関数を修正しました

--- ranking_display.php
+++ ranking_display.php
@@ -1,33 +1,33 @@
 <?php
 require("myPDO.php");
 
-$disp_num = $_POST['optval'] ?? '';
+$disp_num = $_POST['opt'] ?? '';
 
 //rankingテーブルからデータを取得
 $db = new myPDO();
 $sql = "SELECT user_name, count FROM ranking INNER JOIN user ON ranking.user_id = user.id ORDER BY count ASC";
 
-$result = $db->query($sql);
+$result = $db->inputSQL($sql);
 
 $rankings = [];
 
-//上位10件を取得
-for ($i=0; $i < $disp_num; $i++){
-    $row = $result[$i];
-    // データが存在する場合のみ配列に追加
-    if ($row) {
+
+// 上位件数を取得
+for ($i = 0; $i < $disp_num; $i++) {
+    if (isset($result[$i])) {
         $rankings[] = [
             'rank' => $i + 1,
-            'user_name' => htmlspecialchars($row['user_name']),
-            'count' => htmlspecialchars($row['count'])
+            'username' => htmlspecialchars($result[$i]['user_name']), // 修正
+            'count' => htmlspecialchars($result[$i]['count'])
         ];
     } else {
-        break; // データがなくなったらループを抜ける
+        break;
     }
 }
 
 // JSON形式でランキングデータを返す
-echo json_encode($rankings);
 header('Content-Type: application/json');
+echo json_encode($rankings);
+
 
 ?>
